4e4fa2b4647ec4439f337742743ba0972fc638d6
ivan <amorim-ivan@hotmail.com>
2023-08-15 22:10:26 -0300

    ajuste layout com bootstrap e icons

--- app.js
+++ app.js
@@ -7,7 +7,28 @@ const cellSize = 20; // Tamanho da célula da grade
 const gridSizeX = Math.ceil(canvas.width / cellSize);
 const gridSizeY = Math.ceil(canvas.height / cellSize);
 
-const path = []; // Caminho único
+// Path inicial
+const path = [
+    {
+        x: 200,
+        y: 221,
+    },
+    {
+        x: 254,
+        y: 458,
+    },
+    {
+        x: 528,
+        y: 424,
+    },
+    {
+        x: 552,
+        y: 148,
+    },
+];
+
+// Caminho único
+const pathGoogleMaps = [];
 
 const DRONES_ENERGY = 1300;
 let dronePathIndex = 0; // Índice atual no caminho
@@ -18,10 +39,10 @@ let isMovingForward = true;
 // Drone infos
 const chargeDuration = 300; // Tempo de recarga em quadros
 const droneCoverage = 5; // Quantidade de quadrados monitorados por cada drone
-const drones = [];
 const enemies = [];
 const newDroneDelay = 50; // Tempo para criar um novo drone em quadros
 let chargeStations = [{ x: 300, y: 50, radius: 20 }];
+let drones = [];
 
 // Google Maps
 const addressInput = document.getElementById('addressInput');
@@ -69,6 +90,7 @@ canvas.addEventListener('click', event => {
     const mouseX = event.clientX - rect.left;
     const mouseY = event.clientY - rect.top;
     path.push({ x: mouseX, y: mouseY });
+
     if (path.length >= 3) {
         isDrawing = false;
         // Verificar se o desenho é um circuito fechado
@@ -89,15 +111,7 @@ canvas.addEventListener('click', event => {
 
 document.getElementById('clearButton').addEventListener('click', () => {
     path.length = 0;
-    drones.length = 1; // Manter apenas um drone
-    drones[0] = {
-        x: chargeStations[0].x,
-        y: chargeStations[0].y,
-        energy: DRONES_ENERGY,
-        speed: 2,
-        isCharging: false,
-        chargeTimer: 0,
-    };
+    drones = [];
     enemies.length = 0;
     isDrawing = false;
     chargeStations = [{ x: 300, y: 50, radius: 20 }];
@@ -121,6 +135,10 @@ canvas.addEventListener('contextmenu', event => {
 function updateEnergyInfo() {
     const energyInfo = document.getElementById('energyInfo');
     energyInfo.innerHTML = '';
+    if (drones?.length <= 0) {
+        energyInfo.innerHTML = 'Sem drones no momento!';
+    }
+
     for (const drone of drones) {
         energyInfo.innerHTML += drone.isCharging
             ? `Drone: Recarregando... <br>`
@@ -141,6 +159,25 @@ function getRandomColor() {
     return color;
 }
 
+function getLatLngFromPixel(x, y, map) {
+    const bounds = map.getBounds();
+    const ne = bounds.getNorthEast();
+    const sw = bounds.getSouthWest();
+    const projection = map.getProjection();
+
+    const topRight = projection.fromLatLngToPoint(ne);
+    const bottomLeft = projection.fromLatLngToPoint(sw);
+
+    const scale = Math.pow(2, map.getZoom());
+    const worldPoint = new google.maps.Point(
+        x / scale + bottomLeft.x,
+        y / scale + topRight.y + 0.2
+    );
+    const latLng = projection.fromPointToLatLng(worldPoint);
+
+    return latLng;
+}
+
 // Inicializar o mapa do google maps
 function initMap() {
     const map = new google.maps.Map(mapDiv, {
@@ -158,11 +195,48 @@ function initMap() {
             }
         });
     });
+
+    // Google MAPS
+    canvas.addEventListener('click', event => {
+        const rect = canvas.getBoundingClientRect();
+        const x = event.clientX - rect.left;
+        const y = event.clientY - rect.top;
+        const latLng = getLatLngFromPixel(x, y, map);
+
+        if ((latLng, map)) {
+            const lat = latLng.lat();
+            const lng = latLng.lng();
+
+            pathGoogleMaps.push({ lat, lng });
+
+            ctx.beginPath();
+            ctx.arc(x, y, 5, 0, 2 * Math.PI);
+            ctx.fillStyle = 'red';
+            ctx.fill();
+
+            const pathCoordinates = pathGoogleMaps.map(
+                point => new google.maps.LatLng(point.lat, point.lng)
+            );
+            /*
+            // TODO pegando os clicks do canvas e desenhando no google maps pegando Lat e Log
+            const pathPolyline = new google.maps.Polyline({
+                path: pathCoordinates,
+                geodesic: true,
+                strokeColor: '#FF0000',
+                strokeOpacity: 1.0,
+                strokeWeight: 2,
+            });
+            console.log('pathCoordinates >> ', pathCoordinates);
+            pathPolyline.setMap(map);
+            */
+        }
+    });
 }
 
 /* Draw */
 function drawGrid() {
     ctx.beginPath();
+    ctx.lineWidth = 1;
     for (let x = 0; x <= canvas.width; x += cellSize) {
         ctx.moveTo(x, 0);
         ctx.lineTo(x, canvas.height);
@@ -171,55 +245,79 @@ function drawGrid() {
         ctx.moveTo(0, y);
         ctx.lineTo(canvas.width, y);
     }
-    ctx.strokeStyle = 'rgba(0, 0, 0, 0.1)'; // Grade mais transparente
+    ctx.strokeStyle = 'rgba(0, 0, 0, 0)'; // Grid fundo mais transparente >> p/ voltar grid ultima casa 0.1
     ctx.stroke();
 }
 
 function redrawCanvas() {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
 
-    ctx.lineWidth = 1;
+    // Desenha Grid
     drawGrid();
 
     // Desenhar estações de recarga
     for (const station of chargeStations) {
-        ctx.lineWidth = 3;
-        ctx.strokeStyle = 'green';
-        ctx.beginPath();
-        ctx.arc(station.x, station.y, station.radius, 0, Math.PI * 2);
-        ctx.stroke();
+        var heightImg = 90;
+        var widthImg = 90;
+        var svgStationImg = document.getElementById('stationPower');
+        ctx.drawImage(
+            svgStationImg,
+            station.x - widthImg / 3,
+            station.y - heightImg / 3,
+            widthImg,
+            heightImg
+        );
     }
 
+    // Desenha o Caminho forma (Linhas)
+    ctx.beginPath();
     ctx.fillStyle = 'blue';
     ctx.strokeStyle = 'blue';
     ctx.lineWidth = 5; // Aumentar a espessura da linha para 5 pixels
     if (path.length > 0) {
-        ctx.beginPath();
+        ctx.setLineDash([10, 15]);
         ctx.moveTo(path[0].x, path[0].y);
         for (const point of path) {
             ctx.lineTo(point.x, point.y);
         }
         ctx.closePath(); // Fechar a forma geométrica
-        ctx.stroke();
     }
+    ctx.stroke();
 
+    // Desenha intersecões clicadas pelo USER (FEEDBACK USER)
+    ctx.fillStyle = 'red';
+    for (const pathPoint of path) {
+        ctx.beginPath();
+        ctx.fillRect(pathPoint.x - 5, pathPoint.y - 5, 10, 10);
+        ctx.fill();
+    }
+    ctx.stroke();
+
+    // Desenha os drones
     for (const drone of drones) {
         if (drone) {
-            ctx.fillStyle = 'red';
-            ctx.beginPath();
-            ctx.arc(drone.x, drone.y, 10, 0, Math.PI * 2);
-            ctx.fill();
+            var heightImg = 45;
+            var widthImg = 45;
+            var svgDroneImg = document.getElementById('drone');
+            ctx.drawImage(
+                svgDroneImg,
+                drone.x - widthImg / 3,
+                drone.y - heightImg / 3,
+                widthImg,
+                heightImg
+            );
         }
     }
 
+    ctx.beginPath();
     ctx.fillStyle = 'yellow';
     for (const enemy of enemies) {
         if (enemy.isAlive) {
-            ctx.beginPath();
             ctx.fillRect(enemy.x - 5, enemy.y - 5, 10, 10);
             ctx.fill();
         }
     }
+    ctx.stroke();
 
     updateEnergyInfo();
     requestAnimationFrame(redrawCanvas);
